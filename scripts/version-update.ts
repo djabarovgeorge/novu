fe629faaafb03c3143854a548de8af0946ac3df8
Gosha <djabarov.george@gmail.com>
2023-10-04 14:41:42 +0300

    feat: update

--- scripts/version-update.ts
+++ scripts/version-update.ts
@@ -20,48 +20,30 @@ try {
       console.error(`Error: ${error.message}`);
   }
   
-  console.log("Step 1.1: Install dependencies");
-  runCommand('pnpm install');
+  // console.log("Step 1.1: Install dependencies");
+  // runCommand('pnpm install');
 
   console.log("Step 2: Push changes in submodule");
-  runCommand('pwd', submoduleDir);
-  // runCommand('cd enterprise/packages');
-  // runCommand('pwd');
-  // runCommand('ls -la');
-
-  runCommand('git status',submoduleDir);
   runCommand('git add .',submoduleDir);
-  runCommand('git status',submoduleDir);
-  
   runCommand('git commit -m "chore: update versions"',submoduleDir);
   runCommand('git push',submoduleDir);
-  // runCommand('cd ../../');
-  // runCommand('pwd');
 
+  console.log("Step 3: Update submodule reference");
+  runCommand('git add enterprise/packages');
+  runCommand('git commit -m "chore: update submodule reference"');
+  runCommand('git push');
 
-  // console.log("Step 2: Push changes in submodule");
-  // runCommand('cd enterprise/packages');
-  // runCommand('git add .');
-  // runCommand('git commit -m "chore: update versions"');
-  // runCommand('git push');
-  // runCommand('cd ../../');
+  console.log("Step 4: Push changes in monorepo versions");
+  runCommand('git add .');
+  runCommand('git commit -m "chore: update versions"');
+  runCommand('git push');
 
-  // console.log("Step 3: Update commit&push submodule reference");
-  // runCommand('git add enterprise/packages');
-  // runCommand('git commit -m "chore: update submodule reference"');
-  // runCommand('git push');
-
-  // console.log("Step 4: Update commit&push monorepo versions");
-  // runCommand('git add .');
-  // runCommand('git commit -m "chore: update versions"');
-  // runCommand('git push');
-
-  // try{
-  //   runCommand('pnpm lerna version patch  --yes');
-  // }
-  // catch(error){  
-  //   console.error(`Error: ${error.message}`);
-  // }
+  try{
+    runCommand('pnpm lerna version patch  --yes');
+  }
+  catch(error){  
+    console.error(`Error: ${error.message}`);
+  }
 } catch (error) {
   console.error(`Error: ${error.message}`);
 }
