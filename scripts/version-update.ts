c87251d9bc707ac99dbaed270a2496a422a70a5e
Gosha <djabarov.george@gmail.com>
2023-10-04 12:31:51 +0300

    feat: add version script

---
+++ scripts/version-update.ts
@@ -0,0 +1,33 @@
+import { execSync } from 'child_process';
+
+function runCommand(command: string, directory?: string) {
+  console.log(`Running command: ${command}`);
+  execSync(command, {
+    stdio: 'inherit',
+    cwd: directory,
+  });
+}
+
+try {
+  console.log("Step 1: Run lerna version in monorepo");
+  runCommand('pnpm lerna version patch');
+
+  console.log("Step 2: Navigate to submodule and update version");
+  runCommand('npm version patch', 'enterprise/packages/digest-schedule');
+
+  console.log("Step 3: Install dependencies and push changes in submodule");
+  runCommand('pnpm install', 'enterprise/packages/digest-schedule');
+  runCommand('git add .', 'enterprise/packages/digest-schedule');
+  runCommand('git commit -m "Update version"', 'enterprise/packages/digest-schedule');
+  runCommand('git push', 'enterprise/packages/digest-schedule');
+
+  console.log("Step 4: Navigate back to monorepo and update submodule reference");
+  runCommand('git add enterprise/packages/digest-schedule');
+  runCommand('git commit -m "Update submodule reference"');
+  runCommand('git push');
+
+  console.log("Step 5: Run lerna version again in monorepo");
+  runCommand('pnpm lerna version patch');
+} catch (error) {
+  console.error(`Error: ${error.message}`);
+}
