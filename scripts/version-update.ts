e35d0ceb0d6be233023c2daae5c8d5093f7bea1b
Gosha <djabarov.george@gmail.com>
2023-10-04 14:23:47 +0300

    feat: update

--- scripts/version-update.ts
+++ scripts/version-update.ts
@@ -21,31 +21,36 @@ try {
   runCommand('pnpm install');
 
   console.log("Step 2: Push changes in submodule");
+  runCommand('pwd');
   runCommand('cd enterprise/packages');
+  runCommand('pwd');
   runCommand('ls -la');
+
   runCommand('git status');
   runCommand('git add .');
   runCommand('git status');
-  runCommand('git commit -m "core: update versions"');
+  
+  runCommand('git commit -m "chore: update versions"');
   runCommand('git push');
   runCommand('cd ../../');
+  runCommand('pwd');
 
 
   // console.log("Step 2: Push changes in submodule");
   // runCommand('cd enterprise/packages');
   // runCommand('git add .');
-  // runCommand('git commit -m "core: update versions"');
+  // runCommand('git commit -m "chore: update versions"');
   // runCommand('git push');
   // runCommand('cd ../../');
 
   // console.log("Step 3: Update commit&push submodule reference");
   // runCommand('git add enterprise/packages');
-  // runCommand('git commit -m "core: update submodule reference"');
+  // runCommand('git commit -m "chore: update submodule reference"');
   // runCommand('git push');
 
   // console.log("Step 4: Update commit&push monorepo versions");
   // runCommand('git add .');
-  // runCommand('git commit -m "core: update versions"');
+  // runCommand('git commit -m "chore: update versions"');
   // runCommand('git push');
 
   // try{
