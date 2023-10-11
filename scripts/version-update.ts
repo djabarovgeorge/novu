98d35b4557a8d711deaa43b464049fb864d23a9e
Gosha <djabarov.george@gmail.com>
2023-10-11 10:58:23 +0300

    chore: update versions

--- scripts/version-update.ts
+++ scripts/version-update.ts
@@ -12,31 +12,47 @@ function runCommand(command: string, directory?: string) {
   });
 }
 
-try {
-  console.log('Step 1: Run lerna version in monorepo without committing or creating a release');
-  runCommand('pnpm lerna version patch --yes --no-git-tag-version --no-push --force-publish');
+(() => {
+  try {
+    console.log('Step 1: Check for uncommitted changes in submodule git');
+    const gitStatus = execSync('git status --porcelain', { cwd: submoduleDir }).toString();
+    if (gitStatus) {
+      console.error(
+        'There are uncommitted changes in the git repository at ' +
+          submoduleDir +
+          ', please commit or stash them before running this script.'
+      );
 
-  console.log('Step 2: Extract new version from lerna json file');
-  const lernaJson = JSON.parse(readFileSync(join(__dirname, '../lerna.json'), 'utf-8'));
-  const newVersion = lernaJson.version;
-  console.log(`New version ${newVersion} extracted`);
+      console.log('The uncommitted changes are:\n' + gitStatus);
 
-  console.log('Step 3: Push changes in submodule');
-  runCommand('git add .', submoduleDir);
-  runCommand('git commit -m "chore: update versions"', submoduleDir);
-  runCommand('git push', submoduleDir);
+      return;
+    }
 
-  console.log('Step 4: Create release tag for submodule');
-  runCommand(`git tag v${newVersion}`, submoduleDir);
-  runCommand('git push --tags', submoduleDir);
+    console.log('Step 2: Run lerna version in monorepo without committing or creating a release');
+    runCommand('pnpm lerna version patch --yes --no-git-tag-version --no-push --force-publish');
 
-  console.log('Step 5: Commit version changes and submodule updated hash main repository');
-  runCommand('git add .');
-  runCommand('git commit -m "chore: update versions"');
+    console.log('Step 3: Extract new version from lerna json file');
+    const lernaJson = JSON.parse(readFileSync(join(__dirname, '../lerna.json'), 'utf-8'));
+    const newVersion = lernaJson.version;
+    console.log(`New version ${newVersion} extracted`);
 
-  console.log('Step 6: Create release tag for main repo and push');
-  runCommand(`git tag v${newVersion}`);
-  runCommand('git push --tags');
-} catch (error) {
-  console.error(`Error: ${error}`);
-}
+    console.log('Step 4: Push changes in submodule');
+    runCommand('git add .', submoduleDir);
+    runCommand('git commit -m "chore: update versions"', submoduleDir);
+    runCommand('git push', submoduleDir);
+
+    console.log('Step 5: Create release tag for submodule');
+    runCommand(`git tag v${newVersion}`, submoduleDir);
+    runCommand('git push --tags', submoduleDir);
+
+    console.log('Step 6: Commit version changes and submodule updated hash main repository');
+    runCommand('git add .');
+    runCommand('git commit -m "chore: update versions"');
+
+    console.log('Step 7: Create release tag for main repo and push');
+    runCommand(`git tag v${newVersion}`);
+    runCommand('git push --tags');
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
+})();
