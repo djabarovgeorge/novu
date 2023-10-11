c15f512d9a7318657982c114df00124c049827ca
Gosha <djabarov.george@gmail.com>
2023-10-11 11:58:35 +0300

    feat: update

--- scripts/version-update.ts
+++ scripts/version-update.ts
@@ -14,47 +14,63 @@ function runCommand(command: string, directory?: string) {
   });
 }
 
+const validateGitWorkingTree = (path?: string) => {
+  const options = path ? { cwd: path } : {};
+
+  const gitStatus = execSync('git status --porcelain', options).toString();
+  if (gitStatus) {
+    console.error(
+      'Working tree has uncommitted changes' +
+        submoduleDir +
+        ', please commit or remove the following changes before continuing:\n' +
+        gitStatus
+    );
+
+    throw new Error('Working tree has uncommitted changes');
+  } else {
+    console.log('No uncommitted changes found');
+  }
+};
+
 (() => {
   try {
+    /*
+     * This step is necessary as the lerna version command lacks the capability to validate the Git tree of submodules,
+     * thus requiring this manual procedure.
+     */
     console.log('Step 1: Check for uncommitted changes in submodule git');
-    const gitStatus = execSync('git status --porcelain', { cwd: submoduleDir }).toString();
-    if (gitStatus) {
-      console.error(
-        'Working tree has uncommitted changes in submodule git' +
-          submoduleDir +
-          ', please commit or remove the following changes before continuing:\n' +
-          gitStatus
-      );
+    validateGitWorkingTree(submoduleDir);
 
-      return;
-    } else {
-      console.log('No uncommitted changes in submodule git found');
-    }
+    /*
+     * This step is essential due to the utilization of the -no-git-tag-version flag in the lerna version command.
+     * The flag prevents Lerna from validating the Git tree, hence necessitating this manual intervention.
+     */
+    console.log('Step 2: Check for uncommitted changes in main git');
+    validateGitWorkingTree();
 
-    console.log('Step 2: Run lerna version in monorepo without committing or creating a release');
-    runCommand('pnpm lerna version patch --yes --no-push');
-    // runCommand('pnpm lerna version patch --yes --no-git-tag-version --no-push');
+    console.log('Step 3: Run lerna version in monorepo without committing or creating a release');
+    runCommand('pnpm lerna version patch --yes --no-git-tag-version --no-push');
 
-    console.log('Step 3: Extract new version from lerna json file');
+    console.log('Step 4: Extract new version from lerna json file');
     const lernaJson = JSON.parse(readFileSync(join(__dirname, '../lerna.json'), 'utf-8'));
     const newVersion = lernaJson.version;
     console.log(`New version ${newVersion} extracted`);
 
-    console.log('Step 4: Push changes in submodule');
+    console.log('Step 5: Push changes in submodule');
     runCommand('git add .', submoduleDir);
     runCommand('git commit -m "chore: update versions"', submoduleDir);
     runCommand('git push', submoduleDir);
 
-    console.log('Step 5: Create release tag for submodule');
+    console.log('Step 6: Create release tag for submodule');
     runCommand(`git tag v${newVersion}`, submoduleDir);
     runCommand('git push --tags', submoduleDir);
 
-    console.log('Step 6: Commit version changes and submodule updated hash main repository');
+    console.log('Step 7: Commit version changes and submodule updated hash main repository');
     runCommand('git add .');
     runCommand('git commit -m "chore: update versions"');
     runCommand('git push');
 
-    console.log('Step 7: Create release tag for main repo and push');
+    console.log('Step 8: Create release tag for main repo and push');
     runCommand(`git tag v${newVersion}`);
     runCommand('git push --tags');
   } catch (error) {
